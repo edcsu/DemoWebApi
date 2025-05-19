9adf627647fb5b0bd31ede0497927efbf484957d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-05-19 20:28:08 +0300

    Revert "feat: add seed data"
    This reverts commit 78b341daa58fe1ce06d69f237111856421a3f7bf.

--- DemoWebApi/Data/Seeder.cs
+++ DemoWebApi/Data/Seeder.cs
@@ -1,6 +1,3 @@
-using Bogus;
-using DemoWebApi.Helpers;
-using DemoWebApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DemoWebApi.Data;
@@ -12,59 +9,5 @@ public class Seeder
         using var serviceScope = app.ApplicationServices.CreateScope();
         var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         context.Database.Migrate();
-        
-        if (!context.Users.Any(a => a.Role == AppRole.SuperAdmin))
-        {
-            var admin = new User()
-            {
-                Id = ApiConstants.SuperAdminId,
-                Email = ApiConstants.SuperAdminEmail,
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword(ApiConstants.SuperAdminPassword),
-                CreatedAt = DateTime.UtcNow.AddYears(-10),
-                UpdatedAt = DateTime.UtcNow.AddYears(-2)
-            };
-
-            context.Users.Add(admin);
-            context.SaveChanges();
-        }
-        
-        if (!context.Users.Any(u => (u.Role == AppRole.Driver) || (u.Role == AppRole.Rider) || (u.Role == AppRole.Admin) ))
-        {
-            var userFaker = new Faker<User>()
-                .RuleFor(x => x.Id, _ => Guid.NewGuid())
-                .RuleFor(x => x.FullName, f => f.Person.FullName)
-                .RuleFor(x => x.Email, (f, u) => f.Internet.Email(u.FullName.ToLower()))
-                .RuleFor(x => x.PhoneNumber, f => f.Person.Phone)
-                .RuleFor(x => x.PasswordHash, f => BCrypt.Net.BCrypt.HashPassword(f.Random.Word()))
-                .RuleFor(x => x.Role, f => f.PickRandomWithout(AppRole.SuperAdmin))
-                .RuleFor(x => x.Rating, (f, u) => u.Role is AppRole.Driver ? f.Random.Int(1,5) : 0)
-                .RuleFor(x => x.CreatedAt, f => f.Date.Past(10).ToUniversalTime())
-                .RuleFor(x => x.UpdatedAt, f => f.Date.Recent(2).ToUniversalTime());
-            var users = userFaker.Generate(100);
-            
-            var driver = new User()
-            {
-                Id = Guid.NewGuid(),
-                Email = ApiConstants.SeedDriverEmail,
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword(ApiConstants.SeedDriverPassword),
-                CreatedAt = DateTime.UtcNow.AddYears(-10),
-                UpdatedAt = DateTime.UtcNow.AddYears(-8)
-            };
-            
-            var client = new User()
-            {
-                Id = Guid.NewGuid(),
-                Email = ApiConstants.SeedClientEmail,
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword(ApiConstants.SeedClientPassword),
-                CreatedAt = DateTime.UtcNow.AddYears(-10),
-                UpdatedAt = DateTime.UtcNow.AddYears(-9)
-            };
-            
-            users.Add(driver);
-            users.Add(client);
-            
-            context.Users.AddRange(users);
-            context.SaveChanges();
-        }
     }
 }
--- DemoWebApi/Services/DriverService.cs
+++ DemoWebApi/Services/DriverService.cs
@@ -20,7 +20,7 @@ public class DriverService : IDriverService
     public async Task<List<DriverInfo>> GetAvailableDrivers()
     {
         return await _context.Users
-            .Where(d => (d.Role == AppRole.Driver))
+            .Where(d => d.Role == AppRole.Rider)
             .Select(d => d.MapToDriverInfo())
             .ToListAsync();
     }
